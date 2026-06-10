ea98b51e7fcfb1666d4be7f0d6484dc39826d6d7
classykss <ksssan007@gmail.com>
2026-06-10 14:24:04 +0530

    version2

--- simple.py
+++ simple.py
@@ -3,16 +3,6 @@ print("Subtraction",7-4)
 print("Multiplication",3*5)
 print("Division",10/2)
 
-
-// factorial of a number
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)   
-print("Factorial of 5 is", factorial(5))
-
-
-// create a list of numbers and print the sum
-numbers = [1, 2, 3, 4, 5]   
-print("Sum of numbers is", sum(numbers))
+//new 
+print("Exponentiation",2**3)
+print("Floor Division",10//3)
